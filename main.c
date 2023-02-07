4525346d63c9aa3d3df8e6b715b5cf6933e785db
Blindeen <kuba.szmajda@gmail.com>
2023-02-07 19:18:27 +0100

    add exemplary code in main

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include "heap.h"
 
 int main() {
-    printf("Hello, World!\n");
+    heap_setup();
+
+    void *ptr = heap_malloc(10); //instead of malloc we can use calloc to set 0 for allocated memory
+    if(ptr == NULL)
+    {
+        return -1;
+    }
+
+    void *tmp = heap_realloc(ptr, 100);
+    if(tmp == NULL)
+    {
+        heap_free(ptr);
+        return -1;
+    }
+    ptr = tmp;
+
+    heap_free(ptr);
+
+    heap_clean();
     return 0;
 }
